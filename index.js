f270619b69a728d7055b78726ec694940d3b470a
徐梓昊 <adherentxu@gmail.com>
2019-01-08 21:23:40 +0800

    add

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // 首字母大写
 const capitalize = (str: string) => {
-	return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+const wait = (ms: number) => new Promise((r, j) => setTimeout(r, ms));
+
