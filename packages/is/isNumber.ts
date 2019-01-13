1ad595610a07389ccf326c17f8759fefd4d79bf1
徐梓昊 <adherentxu@gmail.com>
2019-01-13 17:03:43 +0800

    feat add is floder

---
+++ packages/is/isNumber.ts
@@ -0,0 +1,11 @@
+const isNumber = (num: any): boolean => {
+  if(typeof num === 'number'){
+    return num - num === 0;
+  }
+  if(typeof num === 'string' && num.trim() !== ''){
+    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num)
+  }
+  return false
+}
+
+export default isNumber;
